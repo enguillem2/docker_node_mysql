bfebb633c88ce6feb68b2d4982d30e1bfb71c339
Tu Nombre <you@example.com>
2023-09-02 06:40:40 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import { createPool } from 'mysql2/promise'
+import {config} from 'dotenv'
+config()
+
+const app=express()
+
+const pool=createPool({
+    host:process.env.MYSQLDB_HOST,
+    user:process.env.MYSQLDB_USER,
+    password:process.env.MYSQLDB_ROOT_PASSWORD,
+    port:process.env.MYSQLDB_PORT
+})
+
+app.get('/',(req,res)=>{
+    res.send("hola edmundo")
+})
+
+app.get('/ping',async (req,res)=>{
+    const result=await pool.query("select now()")
+    res.json(result[0])
+
+})
+
+app.listen(3000)
+
+console.log("server on port",3000)
